b3df051cdfd6c8c123d795ba857b62d9bac3584c
Sanikant Kushwaha <141299594+sanikant20@users.noreply.github.com>
2024-04-16 23:45:24 +0545

    deleted unwanted files

--- Development/customer/src/Components/KhaltiPayment/KhaltiKey.js
+++
@@ -1,5 +0,0 @@
-const myKhaltiKey = {
-    sercretKey: 'test_secret_key_f15266e7ae68430897d79a8f4eb96aa4',
-    publicTestKey: 'test_public_key_dd74081676894eaaaad05777b39ebb8c'
-};
-export default myKhaltiKey;
