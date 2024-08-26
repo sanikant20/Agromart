626217f87bac7e422e337a0cc1c26534c07f586c
Sanikant Kushwaha <141299594+sanikant20@users.noreply.github.com>
2024-08-26 20:00:35 +0545

    updated

--- Development/customer/apiconfig.js
+++ Development/customer/apiconfig.js
@@ -1,7 +1,6 @@
 // Base api url with ipv4 Address:: Change Ipv4 addres according to the system ipv4 Address::
 
-const apiUrl = 'http://192.168.254.1:5000/api'; // Home Ipv4 Address
-// const apiUrl = 'http://172.16.19.211:5000/api'; // Clz Ipv4 Address
+const apiUrl = 'http://192.168.254.5:5000/api'; // Home Ipv4 Address
 
 export default apiUrl;
   
